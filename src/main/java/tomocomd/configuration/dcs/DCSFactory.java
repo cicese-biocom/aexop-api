64c1d90ff35fdd03de97a69166936f4ce35ca748
luis <31668585+lgarciaag89@users.noreply.github.com>
2024-09-15 15:34:08 -0700

    add operators, filters and evaluators

--- src/main/java/tomocomd/configuration/dcs/DCSFactory.java
+++ src/main/java/tomocomd/configuration/dcs/DCSFactory.java
@@ -4,7 +4,7 @@ import tomocomd.configuration.dcs.startpep.StartpepDCS;
 
 public class DCSFactory {
 
-    private DCSFactory() {}
+  private DCSFactory() {}
 
   public static AAttributeDCS getDcs(PDType type) {
     if (type == null) throw new IllegalArgumentException("Null DCS not allowed");
--- src/test/java/tomocomd/subsetsearch/evolutive/crossover/UniformCrossoverTest.java
+++ src/test/java/tomocomd/subsetsearch/evolutive/crossover/UniformCrossoverTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import tomocomd.configuration.dcs.AHeadEntity;
 import tomocomd.configuration.dcs.HeadFactory;
 import tomocomd.configuration.dcs.PDType;
+import tomocomd.configuration.subsetsearch.operators.GACrossoverConf;
+import tomocomd.configuration.subsetsearch.operators.GACrossoverType;
 import tomocomd.exceptions.AExOpDCSException;
 
 class UniformCrossoverTest {
@@ -24,8 +26,8 @@ class UniformCrossoverTest {
     AHeadEntity head1 = HeadFactory.getHead(PDType.STARTPEP);
     AHeadEntity head2 = HeadFactory.getHead(PDType.STARTPEP);
 
-    head1.setFromString("MIC_S_T_ptt");
-    head2.setFromString("TIC_T_ptt");
+    head1.setFromString("MIC_S_T_gcp1");
+    head2.setFromString("TIC_A_ptt");
 
     List<AHeadEntity> children = uniformCrossover.makeCrossover(head1, head2);
 
